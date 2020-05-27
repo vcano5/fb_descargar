7bbb3deb096df4618b178718806b64b6df118587
vcano5 <30887270+vcano5@users.noreply.github.com>
2020-05-27 13:51:50 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.listen(process.env.PORT || 8080, ()=> {
 app.get('/descargar', (req, res) => {
 	require('fb-video-downloader').getInfo(req.query.video).then(function(info) {
 		if(info['download'].length > 1) {
-			res.send({"messages": [ { "attachment": { "type": "video", "payload": { "url": info['download']['hd'] } }}] })
+			res.send({"messages": [ { "attachment": { "type": "video", "payload": { "url": info['download']['hd'] } }}, {text: info['download']['hd']}] })
 		}
 		else {
-			res.send({"messages": [ { "attachment": { "type": "video", "payload": { "url": info['download']['sd'] } }}] })
+			res.send({"messages": [ { "attachment": { "type": "video", "payload": { "url": info['download']['sd'] } }}, {text: info['download']['sd']}] })
 		}
 		console.log(JSON.stringify(info, null, 2));
 	})
